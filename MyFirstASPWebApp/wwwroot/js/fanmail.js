3b239fd4039adb86d2d11b18c182841d2cfb1289
Parker Warner <plouwarner@gmail.com>
2023-01-25 18:10:36 -0700

    Add project files.

---
+++ MyFirstASPWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,8 @@
+﻿$("#btnSend").click( function () {
+    alert("From: " + $("#txtFrom").val() +
+        "\nSubject: " + $("#txtSubj").val() +
+        "\nMessage: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle();
+})
+
