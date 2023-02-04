08976135d203657e3c8f6f700de28a47cb02af05
Parker Warner <plouwarner@gmail.com>
2023-02-03 18:09:41 -0700

    MVC Change

---
+++ MyFirstASPWebApp/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FanMail()
+        {
+            return View();
+        }
+    }
+}
